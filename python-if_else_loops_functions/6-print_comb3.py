c3d5a33ee4a22e5d1b58f1c03ab8e93bcab472b1
sindi702 <hamatajsindi@gmail.com>
2023-05-30 15:47:27 -0700

    test

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ for first_digit in range(10):
     for second_digit in range(first_digit, 10):
         if first_digit != second_digit:
             number = (first_digit * 10) + second_digit
-            print("{:02}".format(number))
+            print("{:02}".format(number),",", end="")
