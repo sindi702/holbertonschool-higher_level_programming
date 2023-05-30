a1d31ee661f94efc5665a113c6d80f9fb60f3b09
sindi702 <hamatajsindi@gmail.com>
2023-05-30 15:00:51 -0700

    test

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for num in range(100):
-    if num >=0 & num <10:
+    if num >= 0 & num < 10:
         formatted_num = "{:02}".format(num)
-        print(formatted_num,",", end="")
-
+        print(formatted_num, ",", end="")
