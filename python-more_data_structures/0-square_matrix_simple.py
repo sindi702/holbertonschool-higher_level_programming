8cd8d15851f37736b05e0bb1100d2b95cdd94a0e
sindi702 <hamatajsindi@gmail.com>
2023-06-08 00:18:12 -0700

    test

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if matrix is not None:
+        new_matrix = [row[:] for row in matrix]
+         for i in range(len(new_matrix)):
+              for j in range(len(new_matrix[i])):
+                  new_matrix[i][j] = new_matrix[i][j] * new_matrix[i][j]
+         return new_matrix
