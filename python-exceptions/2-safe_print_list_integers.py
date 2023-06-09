1c354601cb5f3c9fceca0aa0fc27f9b1a9d50bbf
sindi702 <hamatajsindi@gmail.com>
2023-06-09 07:12:15 -0700

    fix

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,7 @@
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     i = 0
-    while i < 0:
+    while i < x:
         try:
             print("{:d}".format(my_list[i]), end="")
             i += 1
