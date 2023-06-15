ce5f976697e4736fcfe2dcbbd896560f0a7e5ae7
sindi702 <hamatajsindi@gmail.com>
2023-06-15 03:48:41 -0700

    fix

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Text indentation module"""
+
+
+def text_indentation(text):
+    """Indents text whenever needed"""
+    if type(text) is not str:
+        raise TypeError('text must be a string')
+    char = ['.', '?', ':']
+    i = 0
+    while i in range(len(text)):
+        print('{}'.format(text[i]), end="")
+        if text[i] in char:
+            print('\n')
+            while text[i + 1] == " ":
+                i += 1
+        i += 1
