689f60bdb22060b5b2faf77b3278677e7d4dc872
sindi702 <hamatajsindi@gmail.com>
2023-06-15 03:41:52 -0700

    fix

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Print square module"""
+
+
+def print_square(size):
+    """Prints a square from the value of size"""
+    if type(size) is not int:
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    if type(size) is float and size < 0:
+        raise TypeError('size must be an integer')
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+            print()
