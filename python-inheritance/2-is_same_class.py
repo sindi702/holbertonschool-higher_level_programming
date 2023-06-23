bb746572da05c51fe0a8b74a91bc1558d5a9fc33
sindi702 <hamatajsindi@gmail.com>
2023-06-23 04:06:39 -0700

    inheritance

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Module for a func'''
+
+
+def is_same_class(obj, a_class):
+    '''fun that find the type'''
+    if type(obj) is a_class:
+        return True
+    return False
