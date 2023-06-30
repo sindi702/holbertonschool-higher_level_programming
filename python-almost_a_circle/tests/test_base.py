160879cd6ac8ab37460ad305426754950f240a97
sindi702 <hamatajsindi@gmail.com>
2023-06-30 12:57:04 -0700

    fix

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Test module"""
-import io, sys, unittest, os
+import io, sys, unittest
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
