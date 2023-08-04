e2322b81ad23b787c5e9b1d418c0ed0dd9b71f22
sindi702 <hamatajsindi@gmail.com>
2023-08-04 01:51:41 -0700

    test

--- 0-select_states.py
+++ 0-select_states.py
@@ -8,8 +8,8 @@ import sys
 if __name__ == '__main__':
 
     db = MySQLdb.connect(host="localhost", port=3306,
-                        user=sys.argv[1], passwd=sys.argv[2],
-                        db=sys.argv[3], charset="utf8")
+            user=sys.argv[1], passwd=sys.argv[2],
+            db=sys.argv[3], charset="utf8")
 
     cr = db.cursor()
     cr.execute("SELECT * FROM states ORDER BY states.id")
